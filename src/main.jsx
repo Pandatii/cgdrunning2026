dbf612be11b4814e68c0b8a0a4da179f142cafca
panda <pandatii@gmail.com>
2026-09-04 21:51:25 +0700

    fixed bug

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import './index.css'
+
+// ใส่ Link ที่เตรียมไว้ตรงนี้
+const TARGET_URL = 'https://script.google.com/macros/s/AKfycbzOtKUBKZzIJNh3fqfaAGrf-0hXmM-Io6P1R6LBn2l85rrbcoOnKwTEQf0Wwu7Dufy1/exec';
+
+// สั่ง Redirect ทันที
+window.location.replace(TARGET_URL);
